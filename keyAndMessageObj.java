bfb3a4b99c430924ac4eea39f4dc14f610d84298
Adam McDonald <117361557+AdamRoMcDonald@users.noreply.github.com>
2023-06-28 16:20:28 -0400

    Update keyAndMessageObj.java
    This version of keyAndMessageObj removes the need for a separate CaesarCipher class. 
    
    It decrypts/encrypts in this Class now. It also does this differently, instead of creating a dictionary of alphabetical characters with keys numbered 0-25, it simply splits the message into an array of individual letter strings and casts them into corresponding ASCII Integers, adds the key to each int, and then casts the elements back into the new letters and uses a StringBuilder to construct them into a new word.

--- keyAndMessageObj.java
+++ keyAndMessageObj.java
@@ -2,79 +2,96 @@ import java.util.Scanner;
 
 /**
  * @author Adam McDonald
- * @version 0.1
- * </p> This class stores the key and message in an object.
+ * @version 0.2
+ * <p>This class creates a new object which holds a string message (from user input) and numerical key from the same source.
+ * It also has the actual method for decoding or encoding a message with the Caesar cipher.</p>
  */
-
-public class keyAndMessageObj {
-    /**
-     * String message represents an alphabetical message which the user gives, either to be decrypted or encrypted.
-     */
-    public String message;
-
-    /**
-     * int key represents the key of the Cipher, in other words the number of letters in the alphabet that each letter in the message will be shifted.
-     */
-    public int key;
-
-    /**
-     * Default constructor for this class.
-     */
-
-    public keyAndMessageObj(){
+class MessageAndKeyObj{
+    String message;
+    int key;
+    private Set<String> dictionary;
+    public MessageAndKeyObj(){
         this.message = message;
         this.key = key;
     }
-
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public int getKey() {
+        return key;
+    }
     public void setKey(int key) {
         this.key = key;
     }
 
     /**
-     * getKey() is never used.
+     * public void EnglishWordChecker takes a String for a filepath name and loads it to a sorted Set<String>
+     * @param dictionaryFilePath A string which represents the name of the file from which words are being drawn. In this case, the filename is "words.txt".
+     * @throws IOException File not found exception.
+     * @see https://github.com/dwyl/english-words
      */
-
-    public int getKey() {
-        return key;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    /**
-     * getMessage() is never used.
-     */
-
-    public String getMessage() {
-        return message;
+    public void EnglishWordChecker(String dictionaryFilePath) throws IOException {
+        dictionary = loadDictionary(dictionaryFilePath);
     }
 
     /**
-     * <p>public void welcome(Scanner scanObj) is essentially the UI driver. It asks the user which function they would like to use and then directs
-     * the user to the proper questions for their choice.
-     * </p>
-     * @param scanObj This is a global Scanner object for this project, can be found in public class Driver
+     * public Set<String> loadDictionary(String dictionaryFilePath) Initializes the dictionary Set<> and populates it with all words from the reference file. (words.txt)
+     * @param dictionaryFilePath
+     * @return
+     * @throws IOException
      */
-    public void welcome(Scanner scanObj){
-        System.out.println("Welcome to the Caesar Cipher Tool"+'\n'+"What would you like to do:"+'\n'+"1. Encrypt a message"+'\n'+"2. Decrypt a message"+'\n');
-        String answer = scanObj.nextLine();
-        switch (answer){
-            case "1":
-                Encrypt en = new Encrypt(scanObj);
-                break;
+    public Set<String> loadDictionary(String dictionaryFilePath) throws IOException {
+        Set<String> dictionary = new HashSet<>();
+        BufferedReader reader = new BufferedReader(new FileReader(dictionaryFilePath));
+        String line;
 
-            case "2":
-                Decrypt de = new Decrypt(scanObj);
-                break;
+        /**
+         * Sets all letters to lowercase and trims for ease of processing.
+         */
 
-            default:
-                System.out.println("Invalid choice, please enter either 1 or 2");
-                break;
+        while ((line = reader.readLine()) != null) {
+            dictionary.add(line.trim().toLowerCase());
         }
+        reader.close();
+        return dictionary;
+    }
 
+    /**
+     * public boolean isEnglishWord very simply checks if the given message (from user) is present in the dictionary Set and would thus be an English word.
+     * @param word Represents the user message.
+     * @return Returns true if the user message is an English word, and false if not.
+     */
+    public boolean isEnglishWord(String word) {
+        String lowercaseWord = word.toLowerCase();
+        return dictionary.contains(lowercaseWord);
     }
 
 
-}
+    /**
+     * This is the method which actually does the ciphering/deciphering.
+     * @param message
+     * @param key
+     * @return
+     * @throws UnsupportedEncodingException
+     * <p>Splits the user message into an array of single letter strings, changes each element in the array into the corresponding ASCII number,
+     * adds the key to said number, turns each individual element back into the new corresponding ASCII character, and then reconstructs the word in a StringBuilder object.
+     */
+
+
+    public String CaesarCipher(String message, int key) throws UnsupportedEncodingException {
+        String[] test = message.split("");
+        StringBuilder sB = new StringBuilder();
+        for (String c : test) {
+            char a = c.charAt(0);
+            int ascii = (int)a+key;
+            char asciiToChar = (char) ascii;
+            sB.append(asciiToChar);
+        }
+        return sB.toString();
+        }
+    }
+
 
