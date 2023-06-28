1ead51effcbc2a2347549c5f2999325174f0bf78
Adam McDonald <117361557+AdamRoMcDonald@users.noreply.github.com>
2023-06-28 16:29:17 -0400

    Update Decrypt.java
    Allows the user to find a hidden message even if they do not have the key. 
    
    Probably the coolest part of the entire project.
    
    Credit and thanks to dwyl's english-words project.
    https://github.com/dwyl/english-words

--- Decrypt.java
+++ Decrypt.java
@@ -2,24 +2,41 @@ import java.util.Scanner;
 
 /**
  * @author Adam McDonald
- * @version 0.1
- * Decryption Option.
+ * @version 0.2
+ * <p>Decryption class deals with encryption choice by user. Gets key and message and sets them.</p>
  */
+class Decrypt{
+    public Decrypt(Scanner scanObj) throws IOException {
 
-public class Decrypt extends keyAndMessageObj{
-    public Decrypt(Scanner scanObj){
-        System.out.println("Please enter the message to decrypt: ");
-        setMessage(scanObj.nextLine());
-        System.out.println("Please enter the desired key: ");
-        int keyForDecrypt = scanObj.nextInt();
+
+        MessageAndKeyObj MKPair = new MessageAndKeyObj();
+        MKPair.EnglishWordChecker("words.txt");
+        System.out.println("Please enter the message to de-encrypt: ");
+        MKPair.setMessage(scanObj.nextLine());
+
         /**
-         * To de-encrypt a message, the opposite key must be used.
+         *If user does not know the key, this code iterates through all possible keys (26) and checks them using the dictionary.
+         *@see The loadDictionary, isEnglishWord, and EnglishWordChecker methods in keyAndMessageObj class.
          */
-        setKey(-keyForDecrypt);
-
-        scanObj.nextLine();
-        CaesarCipher CC = new CaesarCipher(getMessage(),getKey());
-
-
+        
+        System.out.println("Do u know key? Y/N: ");
+        String knowKeyAnswer = scanObj.nextLine();
+        switch (knowKeyAnswer) {
+            case "N" -> {
+                for (int i = 0; i <= 26; i++) {
+                    if (MKPair.isEnglishWord(MKPair.CaesarCipher(MKPair.getMessage(), -i))) {
+                        System.out.println("It appears that your message, " + MKPair.getMessage() + " turns into: " + MKPair.CaesarCipher(MKPair.getMessage(), -i));
+                    }
+                }
+            }
+            case "Y" -> {
+                System.out.println("Please enter the desired key: ");
+                MKPair.setKey(-scanObj.nextInt());
+                scanObj.nextLine();
+                System.out.println("Your message decrypted to a key of "+MKPair.getKey()+" : "+MKPair.CaesarCipher(MKPair.getMessage(), MKPair.getKey()));
+            }
+            default ->
+                    System.out.println("Sorry, that's not a valid input. Please enter either Y for yes, or N for no. ");
+        }
     }
 }
