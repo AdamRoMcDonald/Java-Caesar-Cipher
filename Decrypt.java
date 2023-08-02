8f36802532c01260bdd6fa9fb4290c46818919e1
Adam McDonald <117361557+AdamRoMcDonald@users.noreply.github.com>
2023-08-02 19:56:02 -0400

    Update Decrypt.java
    Grammatical changes.

--- Decrypt.java
+++ Decrypt.java
@@ -21,24 +21,38 @@ public class Decrypt{
          *@see The loadDictionary, isEnglishWord, and EnglishWordChecker methods in keyAndMessageObj class.
          */
         
-        System.out.println("Do u know key? Y/N: ");
-        String knowKeyAnswer = scanObj.nextLine();
-        switch (knowKeyAnswer) {
-            case "N" -> {
-                for (int i = 0; i <= 26; i++) {
-                    if (MKPair.isEnglishWord(MKPair.CaesarCipher(MKPair.getMessage(), -i))) {
-                        System.out.println("It appears that your message, " + MKPair.getMessage() + " turns into: " + MKPair.CaesarCipher(MKPair.getMessage(), -i));
+        boolean idk = true;
+        idk = true;
+        while(idk) {
+            System.out.println("Do you know the key? Y/N: ");
+            String knowKeyAnswer = scanObj.nextLine();
+            switch (knowKeyAnswer) {
+                case "N" -> {
+                    for (int i = 0; i <= 26; i++) {
+                        if (MKPair.isEnglishWord(MKPair.CaesarCipher(MKPair.getMessage(), -i))) {
+                            System.out.println("It appears that your message, " + MKPair.getMessage() + " turns into: " + MKPair.CaesarCipher(MKPair.getMessage(), -i));
+                            idk = false;
+                        }
                     }
                 }
-            }
-            case "Y" -> {
-                System.out.println("Please enter the desired key: ");
-                MKPair.setKey(-scanObj.nextInt());
-                scanObj.nextLine();
-                System.out.println("Your message decrypted to a key of "+MKPair.getKey()+" : "+MKPair.CaesarCipher(MKPair.getMessage(), MKPair.getKey()));
-            }
-            default ->
+                case "Y" -> {
+                    System.out.println("Please enter the desired key: ");
+                    MKPair.setKey(-scanObj.nextInt());
+                    scanObj.nextLine();
+                    System.out.println("Your message decrypted to a key of " + MKPair.getKey() + " : " + MKPair.CaesarCipher(MKPair.getMessage(), MKPair.getKey()));
+                    System.out.println("Would you like to continue to main menu or exit? (E for exit, C for continue): ");
+                    idk = false;
+
+
+                }
+                default -> {
                     System.out.println("Sorry, that's not a valid input. Please enter either Y for yes, or N for no. ");
+                    idk = true;
+                }
+
+
+            }
         }
+        
     }
 }
