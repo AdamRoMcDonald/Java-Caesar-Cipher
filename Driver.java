c8d0a08fab2bc934dade651c95f6eb51c8c79810
Adam McDonald <117361557+AdamRoMcDonald@users.noreply.github.com>
2023-08-02 19:44:54 -0400

    Update Driver.java
    Made the program with an exit or restart option.

--- Driver.java
+++ Driver.java
@@ -13,21 +13,32 @@ import java.util.Set;
  * <p>Driver class acts as an introductory UI.</p>
  */
 
-public class Driver{
+class Driver {
     public static void main(String[] args) throws IOException {
         Scanner scanObj = new Scanner(System.in);
-        System.out.println("Welcome to the tool, choose function\n1. Encrypt\n2. Decrypt");
-        String userChoice = scanObj.nextLine();
+        String restart = "C";
+        while (restart.equals("C")) {
+            System.out.println("Welcome to the tool, choose function\n1. Encrypt\n2. Decrypt");
+            String userChoice = scanObj.nextLine();
 
-        switch (userChoice) {
-            case "1" -> {
-                Encryption en = new Encryption(scanObj);
+            switch (userChoice) {
+                case "1" -> {
+                    Encryption en = new Encryption(scanObj);
+                }
+                case "2" -> {
+                    Decrypt de = new Decrypt(scanObj);
+                }
+                default -> System.out.println("That's not an option");
             }
-            case "2" -> {
-                Decrypt de = new Decrypt(scanObj);
+            System.out.println("Would you like to continue to main menu or exit from the program? (E for exit, C for continue): ");
+            restart = scanObj.nextLine();
+            while(!restart.equals("E") && !restart.equals("C")){
+                System.out.println("Sorry, "+restart+", is not an option. Please choose C for continue or E for exit.");
+                restart = scanObj.nextLine();
             }
-            default -> System.out.println("That's not an option");
+
+
         }
-
+        System.out.println("Thank you for using the Caesar Cipher Solver, have a nice day!");
     }
 }
